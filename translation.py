995a98bb4be37a225b03801c0e6644c7709897f3
Tellyps <111003958+Tellyps@users.noreply.github.com>
2022-12-12 10:34:36 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -3,8 +3,8 @@ class Translation(object):
     ERROR = "<b>ERROR :</b> {}"
 
     START_TEXT = """Hi {},
-Myself Lazy-MissPerfect !
-You can upload all https link including sony urls, voot, etc... & direct links \n\n Thank you @mRiderDM for this awesome bot
+Myself PS-UPLOADER !
+You can upload all https link including sony urls, voot, etc... & direct links \n\n Thank you @PS_MOVIES11 for this awesome bot
 
 /help for more details!"""
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
@@ -13,12 +13,12 @@ URL | filename | username | password"""
     DOWNLOAD_START = "⚡️ **Downloading**..."
     UPLOAD_START = "⬇️ **Uploading**..."
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks for using @\n\n<b>Join : @LazyDeveloper</b>"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks for using @\n\n<b>Join : @PS_MOVIES11</b>"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
     SAVED_CUSTOM_THUMB_NAIL = "Custom thumbnail saved. This image will be used in both video & file ✅."
     DEL_ETED_CUSTOM_THUMB_NAIL = "Custom thumbnail cleared succesfully ✅."
     CUSTOM_CAPTION_UL_FILE = "{}"
-    NO_VOID_FORMAT_FOUND = "ERROR...\n<b>MissPerfect</b> said: {}"
+    NO_VOID_FORMAT_FOUND = "ERROR...\n<b>PSUPLOAD</b> said: {}"
     HELP_USER = """How to Use Me? Follow These steps!
     
     
@@ -30,13 +30,13 @@ URL | filename | username | password"""
    Video  - Give File as video without Screenshots
    File   - Give File without Screenshots
 
-If bot didn't respond, contact @LazyDeveloper or @mRiderDM"""
+If bot didn't respond, contact @PS_MOVIES11 or @PSMOVIESBOT"""
     REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Reply /generatecustomthumbnail to a media album, to generate custom thumbail"
     ERR_ONLY_TWO_MEDIA_IN_ALBUM = """Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
 You can use /rename command after receiving file to rename it with custom thumbnail support.
 """
     CANCEL_STR = "Process Cancelled"
     ZIP_UPLOADED_STR = "Uploaded {} files in {} seconds"
-    SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
+    SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://t.me/PsWhatsAppbot and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
 
-    ERROR_YTDLP = "please report this issue on https://t.me/LazyPrincessSupport ."
+    ERROR_YTDLP = "please report this issue on https://t.me/PSMOVIESBOT ."
